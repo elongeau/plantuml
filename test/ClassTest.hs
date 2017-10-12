e0b83a193dca458effe620a37b7fa513c6aa8837
Emmanuel LONGEAU <external.z12elong@decathlon.com>
2017-10-12 13:53:28 +0200

    fixup! feat(class): generate a very simple diagram

--- test/ClassTest.hs
+++ test/ClassTest.hs
@@ -1,10 +1,37 @@
 module ClassTest where
 
 import Test.Tasty.HUnit
-import PlantUml.ClassDiagram
 
-unit_simpleClass :: IO ()
-unit_simpleClass = classOf "Main" @?= Class "Main"
+data Diagram = Diagram { links :: [(Class, Class)] }
+    deriving (Eq,Show)
+data Class = Class { name :: String }
+    deriving (Eq)
 
-unit_ClassWithProperties :: IO ()
-unit_ClassWithProperties = classProps "User" [("name", "John")] @?= ClassWithProps "User" [("name", "John")]
+instance Show Class where
+    show (Class name) = "class " ++ name
+simpleCase :: Diagram
+simpleCase = 
+    let
+        foo = Class "Foo"
+        bar = Class "Bar"
+    in
+        Diagram [(foo,bar)]
+
+plantUML :: Diagram -> [String]
+plantUML (Diagram links) = 
+    let 
+        first = map (toName fst) links
+        second = map (toName snd) links
+        allLink = map toString links
+    in first ++ second ++ allLink
+        where
+            toName :: ((Class,Class) -> Class) -> (Class, Class) -> String
+            toName f link = show $ f link
+            toString :: (Class, Class) -> String
+            toString (left, right) = show (name left) ++ " o-- " ++ show (name right)
+
+unit_convert_a_diagram_to_a_string :: IO ()
+unit_convert_a_diagram_to_a_string = res @?= unlines ["class Foo","class Bar","Foo o-- Bar"]
+        where
+            res = unlines $ plantUML simpleCase
+                    
