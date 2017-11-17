61f41be06646b2b4d45604d13063b54107bae459
Emmanuel LONGEAU <external.z12elong@decathlon.com>
2017-11-17 22:35:41 +0100

    fixup! feat(class): generate a very simple diagram

--- src/PlantUml/ClassDiagram.hs
+++ src/PlantUml/ClassDiagram.hs
@@ -1,11 +1,1 @@
 module PlantUml.ClassDiagram where
-
-data ClassDiagram = Class {className :: String} 
-    | ClassWithProps {className :: String, props :: [(String,String)]}
-    deriving (Eq, Show)
-
-classOf :: String -> ClassDiagram
-classOf clazz = Class clazz
-
-classProps :: String -> [(String,String)] -> ClassDiagram
-classProps clazz props = ClassWithProps clazz props
--- test/ClassTest.hs
+++ test/ClassTest.hs
@@ -2,36 +2,37 @@ module ClassTest where
 
 import Test.Tasty.HUnit
 
-data Diagram = Diagram { links :: [(Class, Class)] }
-    deriving (Eq,Show)
-data Class = Class { name :: String }
-    deriving (Eq)
+data ClassDiagram = 
+    ClassDiagram [ Entity ]
+
+data Entity = 
+    Box Class
+    | Link Relation
+    
+data Class = 
+    Class { name :: String }
+
+data Relation = 
+    Relation Class Class 
+    deriving Show
+
+classOf :: String -> Class
+classOf name = Class name
+
+box :: Class -> Entity
+box clazz = Box clazz
 
 instance Show Class where
-    show (Class name) = "class " ++ name
-simpleCase :: Diagram
-simpleCase = 
-    let
-        foo = Class "Foo"
-        bar = Class "Bar"
-    in
-        Diagram [(foo,bar)]
+    show (Class name) = "class " ++ name 
 
-plantUML :: Diagram -> [String]
-plantUML (Diagram links) = 
-    let 
-        first = map (toName fst) links
-        second = map (toName snd) links
-        allLink = map toString links
-    in first ++ second ++ allLink
-        where
-            toName :: ((Class,Class) -> Class) -> (Class, Class) -> String
-            toName f link = show $ f link
-            toString :: (Class, Class) -> String
-            toString (left, right) = show (name left) ++ " o-- " ++ show (name right)
+instance Show Entity where 
+    show (Box clazz) = show clazz
+    show (Link rel) = show rel
 
-unit_convert_a_diagram_to_a_string :: IO ()
-unit_convert_a_diagram_to_a_string = res @?= unlines ["class Foo","class Bar","Foo o-- Bar"]
-        where
-            res = unlines $ plantUML simpleCase
-                    
+instance Show ClassDiagram where
+    show (ClassDiagram entities) = unlines $ map show entities
+
+unit_create_a_simple_diagram :: IO ()
+unit_create_a_simple_diagram = 
+    res @?= unlines [ "class Foo" ]
+        where res = show $ ClassDiagram [ box $ classOf "Foo" ]
