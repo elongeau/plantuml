b1d696d39b77ae27b219302a8b1e6214fbfb5ad1
Emmanuel LONGEAU <external.z12elong@decathlon.com>
2017-12-29 18:58:53 +0100

    feat : Redesign to make it a functor

--- src/PlantUml/ClassDiagram.hs
+++ src/PlantUml/ClassDiagram.hs
@@ -1,1 +1,35 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+
 module PlantUml.ClassDiagram where
+
+data Diagram a =
+   Diagram [a]
+   deriving (Show, Eq)
+
+data Class = 
+    Class { className :: String }
+
+data Relation = 
+    Link { from :: Class, to :: Class }
+
+instance Functor Diagram where
+    fmap f (Diagram xs) = Diagram $ fmap f xs
+
+class Plantuml a where
+    toPlantuml :: a -> String
+
+instance Plantuml Class where
+    toPlantuml = 
+        (++) "class " . className
+
+instance Plantuml Relation where
+    toPlantuml (Link (Class f) (Class t)) = 
+        f ++ " -> " ++ t
+data Plantumlable =
+    forall a. Plantuml a => Plantumlable a
+
+instance Plantuml Plantumlable where
+    toPlantuml (Plantumlable a) = toPlantuml a
+
