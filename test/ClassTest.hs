05bba303b842fff62120690cec6d211f7a55c94d
elongeau <emmanuel.longeau@gmail.com>
2017-12-31 14:58:25 +0100

    feat : Redesign to make it a functor (#9)

--- test/ClassTest.hs
+++ test/ClassTest.hs
@@ -1,53 +1,27 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module ClassTest where
 
 import Test.Tasty.HUnit
-
-data ClassDiagram = 
-    ClassDiagram [ Entity ]
-
-data Entity = 
-    Box Class
-    | Link Relation
+import PlantUml.ClassDiagram
     
-data Class = 
-    Class { name :: String }
-
-data Relation = 
-    Relation Class Class 
-
-classOf :: String -> Class
-classOf name = Class name
-
-box :: Class -> Entity
-box clazz = Box clazz
-
-instance Show Class where
-    show (Class name) = "class " ++ name 
-
-instance Show Relation where
-    show (Relation c1 c2) = name c1 ++ " -> " ++ name c2
-
-instance Show Entity where 
-    show (Box clazz) = show clazz
-    show (Link rel) = show rel
-
-instance Show ClassDiagram where
-    show (ClassDiagram entities) = unlines $ map show entities
+instance Plantuml [Char] where
+    toPlantuml = id
 
 unit_create_a_simple_diagram :: IO ()
 unit_create_a_simple_diagram = 
-    res @?= unlines [ "class Foo" ]
-        where res = show $ ClassDiagram [ box $ classOf "Foo" ]
+    res @?= Diagram [ "class Foo" ]
+        where res = toPlantuml <$> Diagram [ Class "Foo" ]
 
 unit_create_two_linked_class :: IO ()
 unit_create_two_linked_class = 
     let
-        foo = classOf "Foo"
-        bar = classOf "Bar"
-        rel = Relation foo bar
-        diag = ClassDiagram [ Box foo, Box bar , Link rel ]
+        foo = Class "Foo"
+        bar = Class "Bar"
+        rel = Plantumlable $ Link foo bar
+        diag = toPlantuml <$> Diagram [ Plantumlable $ foo, Plantumlable $ bar, rel ]
     in 
-        show diag @?= unlines [
+        diag @?= Diagram [
             "class Foo", 
             "class Bar",
             "Foo -> Bar"
