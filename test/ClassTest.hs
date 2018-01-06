1c65e9ee7791aab66b50677d4cabe7325fa49757
Emmanuel LONGEAU <external.z12elong@decathlon.com>
2018-01-06 22:46:39 +0100

    fixup! feat: redesign from scratch

--- test/ClassTest.hs
+++ test/ClassTest.hs
@@ -4,6 +4,8 @@ module ClassTest where
 
 import Test.Tasty.HUnit
 import PlantUml.ClassDiagram
+
+import Control.Monad.State
     
 unit_CreateADiagramWithOneClass :: IO ()
 unit_CreateADiagramWithOneClass = 
@@ -22,6 +24,32 @@ unit_CreateADiagramWithLinkedClass =
         uml = toPlantuml d''
     in
         uml @?= unlines [
+            "class Foo",
+            "class Bar",
+            "Foo -> Bar"
+        ]
+
+type DiagramState a = State Diagram a
+
+clazz :: String -> DiagramState Item
+clazz className =
+    state $ \d -> (Class className, addClass d className)
+
+link :: Item -> Item -> DiagramState Link
+link i1 i2 =
+    state $ \d -> (Link i1 i2, addLink d i1 i2)
+
+unit_CanUseDoNotation :: IO ()
+unit_CanUseDoNotation = 
+    let
+        d = do
+                foo <- clazz "Foo"
+                bar <- clazz "Bar"
+                link foo bar
+        res = execState d initialDiagram
+        uml = toPlantuml res
+    in
+        uml @?= unlines [ 
             "class Foo",
             "class Bar",
             "Foo -> Bar"
