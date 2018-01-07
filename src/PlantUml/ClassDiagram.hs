6f55f7eb155cbc32024160adbf61885b063de183
elongeau <emmanuel.longeau@gmail.com>
2018-01-07 15:18:09 +0100

    refactor: Limit access to function (#15)
    close #12

--- src/PlantUml/ClassDiagram.hs
+++ src/PlantUml/ClassDiagram.hs
@@ -2,7 +2,15 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE InstanceSigs #-}
 
-module PlantUml.ClassDiagram where
+module PlantUml.ClassDiagram (
+    Diagram(Diagram),
+    clazz,
+    link,
+    toPlantuml,
+    initialDiagram
+)
+where
+
 import Control.Monad.State
 
 data Diagram = Diagram {
@@ -14,7 +22,7 @@ data Item =
     Class String
     deriving (Eq, Show)
 
-data Link = 
+data Link =
     Link Item Item
     deriving (Eq, Show)
 
@@ -23,18 +31,18 @@ initialDiagram =
     Diagram [] []
 
 addItem :: Diagram -> Item -> Diagram
-addItem d@(Diagram items _) item = 
+addItem d@(Diagram items _) item =
     d { items = item : items}
-    
+
 addLink :: Diagram -> Link -> Diagram
 addLink d@(Diagram _ links) link =
     d { links = link : links }
 
 class PlantUmlable a where
     toPlantuml :: a -> String
-    
+
 instance PlantUmlable Diagram where
-    toPlantuml (Diagram items links) = 
+    toPlantuml (Diagram items links) =
         let
             items' = toPlantuml items
             links' = toPlantuml links
@@ -42,24 +50,24 @@ instance PlantUmlable Diagram where
             items' ++ links'
 
 instance PlantUmlable a => PlantUmlable [a] where
-    toPlantuml = 
+    toPlantuml =
         unlines . reverse . map toPlantuml
 
 instance PlantUmlable Item where
-    toPlantuml (Class className) = 
+    toPlantuml (Class className) =
         "class " ++ className
 
 instance PlantUmlable Link where
     toPlantuml (Link (Class c1) (Class c2)) =
         c1 ++ " -> " ++ c2
-    
+
 type DiagramState a = State Diagram a
 
 clazz :: String -> DiagramState Item
 clazz className =
-    state $ \d -> 
+    state $ \d ->
         let
-            c = Class className 
+            c = Class className
             d' = addItem d c
         in
             (c, d')
